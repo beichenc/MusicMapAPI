62f53175d3d4adeb59e23283d3d8e2f9dabec9ac
Beichen Chen <beichen@kth.se>
2017-05-08 18:40:17 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::API
   include Response
-  before_filter :cors_set
-  after_filter :cors_set
-
-  def cors_set
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
+  
 end
