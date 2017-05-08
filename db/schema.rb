f7ce34cce527994a45298c68068d996d7053903b
Beichen Chen <beichen@kth.se>
2017-05-08 18:12:13 +0200

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20170508140254) do
   enable_extension "plpgsql"
 
   create_table "mapped_songs", force: :cascade do |t|
-    t.bigint "user_id"
     t.string "username"
     t.string "songname"
     t.string "artist"
@@ -31,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170508140254) do
     t.string "uri"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_mapped_songs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
