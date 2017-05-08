d4d471938322d006eb337fe934915732d199fe01
Beichen Chen <beichen@kth.se>
2017-05-08 17:54:30 +0200

    define users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  # GET
+  def index
+    @users = Users.all
+    json_response(@mapped_songs)
+  end
+
+  # POST
+  def create
+    @user = Users.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :birthday, :producttype, :href, :uri, :imageurl, :followers, :visits)
+  end
 end
