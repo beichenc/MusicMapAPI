daa2b697e74a59c4f046c310aec726765a54e45a
Beichen Chen <beichen@kth.se>
2017-05-09 16:43:10 +0200

    test

--- app/models/mapped_song.rb
+++ app/models/mapped_song.rb
@@ -2,5 +2,5 @@ class MappedSong < ApplicationRecord
 
   # belongs_to :user
 
-  validates_presence_of :username, :songname, :artist, :songimg, :year, :month, :day, :lat, :lng, :uri
+  validates_presence_of :username, :songname, :artist, :genres, :songimg, :year, :month, :day, :timestamp, :lat, :lng, :uri
 end
