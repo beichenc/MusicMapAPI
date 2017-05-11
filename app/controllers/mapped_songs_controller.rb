bcbb5325707f1bbb658c84a5b9a89a490057f1d2
Beichen Chen <beichen@kth.se>
2017-05-11 21:04:19 +0200

    allow displayname

--- app/controllers/mapped_songs_controller.rb
+++ app/controllers/mapped_songs_controller.rb
@@ -14,7 +14,7 @@ class MappedSongsController < ApplicationController
 
   private
   def mapped_song_params
-    params.permit(:username, :songname, :artist, :genres, :songimg, :year, :month, :day, :unixtime, :lat, :lng, :uri)
+    params.permit(:username, :displayname, :songname, :artist, :genres, :songimg, :year, :month, :day, :unixtime, :lat, :lng, :uri)
   end
 
 end
